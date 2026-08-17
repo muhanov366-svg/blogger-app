4086fc6f07169149c43b2a728a75a2514c1ea487
muhanov366-svg <muhanov366@gmail.com>
2026-08-17 16:48:01 +0500

    Add files via upload

---
+++ next-env.d.js
@@ -0,0 +1,7 @@
+/// <reference types="next" />
+/// <reference types="next/image-types/global" />
+import "./.next/types/routes.d.js";
+import "./.next/types/root-params.d.js";
+
+// NOTE: This file should not be edited
+// see https://nextjs.org/docs/app/api-reference/config/typescript for more information.
---
+++ next.config.js
@@ -0,0 +1,7 @@
+// next.config.js
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  // Пустой конфиг — без экспериментальных ключей
+};
+
+module.exports = nextConfig;
